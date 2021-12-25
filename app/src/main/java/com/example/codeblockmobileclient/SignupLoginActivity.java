e5caf773ecc1b9a1940e8efeb9714d422b959449
Ali <alifastith@gmail.com>
2021-12-25 00:39:09 -0500

    dark/light theme colors added, basic themes set up
    still need to add all colors to themes, decide on status bar colors, etc.

---
+++ app/src/main/java/com/example/codeblockmobileclient/SignupLoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.codeblockmobileclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignupLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_login);
+    }
+}
