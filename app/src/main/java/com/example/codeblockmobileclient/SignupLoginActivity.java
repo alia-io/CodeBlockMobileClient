8e54b8429fd28474ddf5482b82a0f8e86c8a0b85
Ali <alifastith@gmail.com>
2021-12-27 22:34:16 -0500

    added signup/login page functionality

--- app/src/main/java/com/example/codeblockmobileclient/SignupLoginActivity.java
+++ app/src/main/java/com/example/codeblockmobileclient/SignupLoginActivity.java
@@ -9,23 +9,48 @@ import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.StyleSpan;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SignupLoginActivity extends AppCompatActivity {
 
+    private enum ActivityOption { SIGNUP, LOGIN };
+    private ActivityOption currentOption;
+
     private TextView instructionView;
+    private TextInputLayout confirmPasswordLayout;
+
+    private TextInputEditText emailInput;
+    private TextInputEditText passwordInput;
+    private TextInputEditText confirmPasswordInput;
+    private Button submitButton;
+
+    private TextView toggleView;
+    private Button toggleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_login);
         instructionView = findViewById(R.id.instruction);
-        setSignupView();
+        confirmPasswordLayout = findViewById(R.id.confirm_password_layout);
+        emailInput = findViewById(R.id.email_address);
+        passwordInput = findViewById(R.id.password);
+        confirmPasswordInput = findViewById(R.id.confirm_password);
+        submitButton = findViewById(R.id.signup_login_button);
+        toggleView = findViewById(R.id.toggle_text);
+        toggleButton = findViewById(R.id.toggle_button);
+        setToLogin();
     }
 
-    private void setSignupView() {
+    private void setToSignup() {
+
         String signupText = getResources().getString(R.string.signup);
-        String signupKeyword = getResources().getString(R.string.signup_keyword);
+        String signupKeyword = getResources().getString(R.string.new_keyword);
         Spannable spannable = new SpannableString(signupText);
         int startIndex = signupText.indexOf(signupKeyword);
         int endIndex = startIndex + signupKeyword.length();
@@ -35,6 +60,75 @@ public class SignupLoginActivity extends AppCompatActivity {
                     R.color.keyword)), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
             spannable.setSpan(new StyleSpan(Typeface.BOLD), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         }
+
         instructionView.setText(spannable);
+        confirmPasswordLayout.setVisibility(View.VISIBLE);
+        submitButton.setText(getResources().getString(R.string.signup_button));
+        toggleView.setText(getResources().getString(R.string.switch_to_login));
+        toggleButton.setText(getResources().getString(R.string.switch_to_login_button));
+        currentOption = ActivityOption.SIGNUP;
+    }
+
+    private void setToLogin() {
+
+        String signupText = getResources().getString(R.string.switch_to_signup);
+        String signupKeyword = getResources().getString(R.string.new_keyword);
+        Spannable spannable = new SpannableString(signupText);
+        int startIndex = signupText.indexOf(signupKeyword);
+        int endIndex = startIndex + signupKeyword.length();
+
+        if (startIndex >= 0) {
+            spannable.setSpan(new ForegroundColorSpan(ContextCompat.getColor(getApplicationContext(),
+                    R.color.keyword)), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            spannable.setSpan(new StyleSpan(Typeface.BOLD), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+
+        instructionView.setText(getResources().getString(R.string.login));
+        confirmPasswordInput.setText("");
+        confirmPasswordLayout.setVisibility(View.GONE);
+        submitButton.setText(getResources().getString(R.string.login_button));
+        toggleView.setText(spannable);
+        toggleButton.setText(getResources().getString(R.string.switch_to_signup_button));
+        currentOption = ActivityOption.LOGIN;
+    }
+
+    public void onClickSubmit(View view) {
+
+        String email = emailInput.getText().toString();
+        String password = passwordInput.getText().toString();
+
+        if (currentOption == ActivityOption.SIGNUP) {
+            String confirmPassword = passwordInput.getText().toString();
+            if (password.equals(confirmPassword)) {
+                // TODO: encrypt, send to server (request new user)
+            } else {
+                // TODO: error text
+            }
+        } else if (currentOption == ActivityOption.LOGIN) {
+            // TODO: encrypt, send to server (request login)
+        }
+    }
+
+    public void onClickToggle(View view) {
+        if (currentOption == ActivityOption.SIGNUP) setToLogin();
+        else if (currentOption == ActivityOption.LOGIN) setToSignup();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
