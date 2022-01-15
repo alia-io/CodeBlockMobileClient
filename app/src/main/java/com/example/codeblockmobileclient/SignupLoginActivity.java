2a1ad5ed22187097e9cf1f97086a03a057cfc9c2
Ali <alifastith@gmail.com>
2022-01-14 19:46:36 -0500

    volley post working

--- app/src/main/java/com/example/codeblockmobileclient/SignupLoginActivity.java
+++ app/src/main/java/com/example/codeblockmobileclient/SignupLoginActivity.java
@@ -22,9 +22,11 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.codeblockmobileclient.communication.dto.AuthPacket;
+import com.example.codeblockmobileclient.communication.dto.LoginForm;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -39,6 +41,8 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -161,10 +165,10 @@ public class SignupLoginActivity extends AppCompatActivity {
                 throw new Exception("JSONObject error");
             }
 
-            //sendHttpPostRequest("login", loginRequestObj);
+            sendHttpPostRequest("login", loginRequestObj);
             //sendHttpGetRequest();
 
-            Executor executor = Executors.newSingleThreadExecutor();
+            /*Executor executor = Executors.newSingleThreadExecutor();
             Handler handler = new Handler(Looper.getMainLooper());    // use for UI stuff
 
             executor.execute(new Runnable() {
@@ -179,7 +183,7 @@ public class SignupLoginActivity extends AppCompatActivity {
                         }
                     });
                 }
-            });
+            });*/
         }
     }
 
@@ -209,136 +213,26 @@ public class SignupLoginActivity extends AppCompatActivity {
         }
     }
 
-    private Boolean sendHttpPostRequest(String urlExt, JSONObject jsonObject) {
-
-        /*try {
-            RequestQueue requestQueue = Volley.newRequestQueue(this);
-            String URL = "http://...";
-            JSONObject jsonBody = new JSONObject();
-            jsonBody.put("Title", "Android Volley Demo");
-            jsonBody.put("Author", "BNK");
-            final String requestBody = jsonBody.toString();
-
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    Log.i("VOLLEY", response);
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Log.e("VOLLEY", error.toString());
-                }
-            }) {
-                @Override
-                public String getBodyContentType() {
-                    return "application/json; charset=utf-8";
-                }
-
-                @Override
-                public byte[] getBody() throws AuthFailureError {
-                    try {
-                        return requestBody == null ? null : requestBody.getBytes("utf-8");
-                    } catch (UnsupportedEncodingException uee) {
-                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
-                        return null;
-                    }
-                }
-
-                @Override
-                protected Response<String> parseNetworkResponse(NetworkResponse response) {
-                    String responseString = "";
-                    if (response != null) {
-                        responseString = String.valueOf(response.statusCode);
-                        // can get more details such as response.headers
-                    }
-                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
-                }
-            };
-
-            requestQueue.add(stringRequest);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }*/
-
-        
+    private void sendHttpPostRequest(String urlExt, JSONObject jsonObject) {
 
         RequestQueue queue = Volley.newRequestQueue(this);
-        Boolean result = false;
-        String urlString = "http://10.0.2.2:8080/" + urlExt;
+        String url = "http://10.0.2.2:8080/" + urlExt;
+        Log.i("Volley", "test");
 
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, urlString, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, jsonObject,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.i("Volley", "Response:\n" + response);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i("Volley", "Error: " + error);
+                    }
+                });
 
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        });
-
-        Request<AuthPacket> request = new Request<AuthPacket>(Request.Method.POST, urlString, null) {
-            @Override
-            protected Response<AuthPacket> parseNetworkResponse(NetworkResponse response) {
-                return null;
-            }
-
-            @Override
-            protected void deliverResponse(AuthPacket response) {
-
-            }
-        };
-
-
-
-
-        /*String urlString = "http://10.0.2.2:8080/" + urlExt;
-        Boolean result = false;
-        HttpURLConnection httpConnection = null;
-
-        try {
-            URL url = new URL(urlString);
-            httpConnection = (HttpURLConnection) url.openConnection();
-            httpConnection.setDoOutput(true);
-            httpConnection.setRequestMethod("POST");
-            httpConnection.addRequestProperty("content-type", "application/json");
-            //httpConnection.setFixedLengthStreamingMode(jsonObject.length());
-            httpConnection.setChunkedStreamingMode(0);
-<<<<<<< HEAD
-            try(OutputStream os = httpConnection.getOutputStream()) {
-                byte[] input = jsonObject.toString().getBytes("utf-8");
-                os.write(input, 0, input.length);
-            }
-            Log.i("HttpPostRequest", "Sending post\n:" + jsonObject.toString());
-=======
-            OutputStream out = httpConnection.getOutputStream();
-            OutputStreamWriter outStream = new OutputStreamWriter(out);
-            outStream.write(jsonObject.toString());
-            Log.i("HttpPostRequest", "Sending post:\n" + jsonObject.toString());
->>>>>>> add volley
-            if (httpConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                InputStream inStream = httpConnection.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inStream));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    Log.d("HttpPostRequest", "line: " + line);
-                }
-                reader.close();
-                Log.i("HttpPostRequest", "POST request returned ok");
-                result = true;
-            } else {
-                Log.i("HttpPostRequest", "POST request returned error");
-                result = false;
-            }
-        } catch (Exception e) {
-            Log.i("HttpPostRequest", "Exception in sendHttpPostRequest");
-            e.printStackTrace();
-            result = false;
-        }*/
-
-        return result;
+        queue.add(request);
     }
 }
 
